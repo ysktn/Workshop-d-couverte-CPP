cd814b4e34ee288b6f808d11684ee53878e019ab
yassin <yassin.ben-younes@epitech.eu>
2025-04-16 14:29:13 +0200

    [CORRECTION] premier exo

--- ex1.cpp
+++ ex1.cpp
@@ -5,31 +5,35 @@
 int main() {
     std::string input;
 
+    // Demander à l'utilisateur d'entrer une chaîne
     std::cout << "Entrez une chaîne de caractères : ";
     std::getline(std::cin, input);
 
-    //afficher la longueur de la chaîne
+    // Afficher la longueur de la chaîne
+    std::cout << "Longueur de la chaîne : " << input.length() << std::endl;
 
+    // Convertir la chaîne en majuscules
     std::string upperCase = input;
-    //changer upperCase pour qu'il soit en majuscules
+    std::transform(upperCase.begin(), upperCase.end(), upperCase.begin(), ::toupper);
     std::cout << "En majuscules : " << upperCase << std::endl;
 
+    // Convertir la chaîne en minuscules
     std::string lowerCase = input;
-    //changer lowerCase pour qu'il soit en minuscules
+    std::transform(lowerCase.begin(), lowerCase.end(), lowerCase.begin(), ::tolower);
     std::cout << "En minuscules : " << lowerCase << std::endl;
 
+    // Inverser la chaîne
     std::string reversed = input;
-    //changer reversed pour qu'il soit inversé
+    std::reverse(reversed.begin(), reversed.end());
     std::cout << "Chaîne inversée : " << reversed << std::endl;
 
+    // Remplacer les espaces par des underscores
     std::string replaced = input;
-    //changer replaced pour qu'il remplace les espaces par des underscores
+    std::replace(replaced.begin(), replaced.end(), ' ', '_');
     std::cout << "Avec espaces remplacés par des underscores : " << replaced << std::endl;
-
-
-    char charToFind = 'a';
     // Trouver la première occurrence d'un caractère spécifique
-    size_t found = input.find(...);
+    char charToFind = 'a';
+    size_t found = input.find(charToFind);
     if (found != std::string::npos) {
         std::cout << "Première occurrence de '" << charToFind << "' trouvée à la position : " << found << std::endl;
     } else {
@@ -38,7 +42,7 @@ int main() {
 
     // Trouver la première occurrence d'une sous-chaîne
     std::string substringToFind = "test";
-    found = input.find(....);
+    found = input.find(substringToFind);
     if (found != std::string::npos) {
         std::cout << "Première occurrence de \"" << substringToFind << "\" trouvée à la position : " << found << std::endl;
     } else {
